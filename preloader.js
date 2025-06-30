b0b480908d820c714de70346f89300d78974f3c7
Leskort <starlaider4@gmail.com>
2025-06-30 22:39:50 +0300

    dadaj

---
+++ preloader.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', function() {
+  window.setTimeout(function() {
+    document.body.classList.add('loaded');
+  }, 1000); // Можно увеличить задержку для теста
+});
